89ed097094b7ca3c10c70a4ec60330e8638a01d2
vinod-vizio <vinod.govindaraj@vizio.com>
2025-11-28 09:35:59 +0530

    Initial Setup

---
+++ PriceTracker/PriceTracker/PriceTrackerApp.swift
@@ -0,0 +1,17 @@
+//
+//  PriceTrackerApp.swift
+//  PriceTracker
+//
+//  Created by Vinodhkumar Govindaraj on 28/11/25.
+//
+
+import SwiftUI
+
+@main
+struct PriceTrackerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
