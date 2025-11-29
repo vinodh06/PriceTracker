88c146cfc83dffc1c6daae5844f074c1bd10b736
vinod-vizio <vinod.govindaraj@vizio.com>
2025-11-29 16:56:42 +0530

    Added deeplink

--- PriceTracker/PriceTracker/Features/StockFeed/Views/StockFeedView.swift
+++ PriceTracker/PriceTracker/Features/StockFeed/Views/StockFeedView.swift
@@ -7,7 +7,7 @@
 
 
 import SwiftUI
-
+/*
 struct StockFeedView: View {
     @State var viewModel: StockFeedViewModel
     
@@ -33,6 +33,50 @@ struct StockFeedView: View {
         }
     }
 }
+*/
+
+struct StockFeedView: View {
+    @State var viewModel: StockFeedViewModel
+    @State private var navigationPath = NavigationPath()
+    
+    init(environment: AppEnvironment = .real) {
+        self.viewModel = StockFeedViewModel(environment: environment)
+    }
+    
+    var body: some View {
+        NavigationStack(path: $navigationPath) {
+            List(viewModel.stocks) { stock in
+                NavigationLink(value: stock) {
+                    StockFeedItemView(stock: stock)
+                }
+            }
+            .animation(.default, value: viewModel.stocks)
+            .navigationBarHidden(true)
+            .safeAreaInset(edge: .top, spacing: 0) {
+                CustomToolbar()
+            }
+            .navigationDestination(for: StockQuote.self) { stock in
+                StockFeedDetailView(stockQuote: stock)
+            }
+            .onOpenURL { url in
+                handleDeepLink(url: url)
+            }
+        }
+        .environment(viewModel)
+    }
+    
+    private func handleDeepLink(url: URL) {
+        guard url.scheme == "stocks",
+              url.host == "symbol" else { return }
+        
+        let tickerSymbol = url.lastPathComponent
+
+        if let stock = viewModel.stocks.first(where: { $0.tickerSymbol == tickerSymbol }) {
+            navigationPath.append(stock)
+        }
+    }
+}
+
 
 struct CustomToolbar: View {
     @Environment(StockFeedViewModel.self) var viewModel
